ebd2e57db51e4b02e280b0f1c6e979496b515213
Sergey Ischenko <si@anahoret.com>
2012-09-10 21:23:39 +0300

    Reworked examples.

--- mix_with_frameworks/js/mix_test.js
+++ mix_with_frameworks/js/mix_test.js
@@ -3,17 +3,21 @@ jQuery.noConflict();
 
 
 (function($) {
-  
+
   $(function() {
     // Set up the "play" buttons for each runnable code example.
-    $('.runnable').each(function() {
+    var bootstrapCode = $('.runnable:first').text();
+
+    $('.runnable').not(':first').each(function() {
       var code = this;
       var $buttonWrapper = $('<div class="button-wrapper"></div>');
       $(code).before($buttonWrapper);
       var $button = $('<div class="run" title="Run"></div>');
-      $button.click(function(){ eval($(code).text()); });
+      $button.click(function(){
+        eval(bootstrapCode + $(code).text());
+      });
       $buttonWrapper.append($button);
     });
   });
-  
+
 })(jQuery);
